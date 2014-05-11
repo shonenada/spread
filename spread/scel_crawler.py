7376d659fa818f8560412d94abc3d806a4cd6fce
shonenada <shonenada@gmail.com>
2014-05-12 01:11:37 +0800

    add a crawler

---
+++ spread/scel_crawler.py
@@ -0,0 +1,116 @@
+#-*- coding: utf-8 -*-
+import re
+import sys
+import Queue
+import urllib
+import threading
+
+import requests
+
+
+def fetch_html(url):
+    res = requests.get(url)
+    if res.status_code == 200:
+        return res.content
+    else:
+        return None
+
+
+class SinglePage(object):
+
+    url_pattern = "http://pinyin.sogou.com/dict/list.php?c=%s&page=%s"
+    href_pattern = re.compile(r'<dt><a href="cell\.php\?id=(?P<id>\d+)">(?P<name>\S+?)</a>[\s\S]+?</dt>\s+?<dd>[\s\S]+?<a href="(?P<url>http://download\.pinyin\.sogou\.com/.+?)" class="dlbtn3">\S+?</a>[\s\S]+?</dd>')
+
+    def __init__(self, category):
+        print "SinglePage: %s" % category
+        self.category = category
+
+    def fetch(self, html):
+        results = self.href_pattern.findall(html)
+        return results
+
+    def fetch_pages(self):
+        page = 1
+        download_links = []
+        while(True):
+            print "Page %s" % page
+            url = self.url_pattern % (self.category, page)
+            html = fetch_html(url)
+            if html is None:
+                break
+            links = self.fetch(html)
+            if len(links) <= 0:
+                print 'Bye %d' % page
+                break
+            download_links.extend(links)
+            page += 1
+
+        return download_links
+
+
+class Downloader(threading.Thread):
+
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            if self.queue.empty():
+                break
+            
+            task = self.queue.get()
+            _id, name, url, path = task
+            target = '%s/%s.scel' % (path, name)
+            print 'from %s to %s' % (url, target)
+            urllib.urlretrieve(url, target)
+            self.queue.task_done()
+
+
+class RangePage(object):
+
+    def __init__(self, save_to, lower=1, upper=500, limit=3):
+        self.lower = lower
+        self.upper = upper
+        self.save_to = save_to
+        self.limit = limit
+        self.dwers = list()
+        self.queue = Queue.Queue()
+
+    def fetch_all(self):
+        for i in xrange(self.lower, self.upper):
+            single_page = SinglePage(i)
+            results = single_page.fetch_pages()
+            for res in results:
+                _id, name, url = res
+                name = name.decode('gb2312').encode('utf-8')
+                self.queue.put((_id, name, url, self.save_to))
+
+        for i in xrange(self.limit):
+            th = Downloader(self.queue)
+            self.dwers.append(th)
+            th.start()
+
+        for dwer in self.dwers:
+            dwer.join()
+
+        self.queue.join()
+
+
+def test():
+    zhirankexue = SinglePage(2)
+    results = zhirankexue.fetch_pages()
+    print results
+
+
+def main():
+    rp = RangePage(save_to='saves', lower=2, upper=3, limit=1)
+    rp.fetch_all()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        test()
+        sys.exit(0)
+
+    main()
